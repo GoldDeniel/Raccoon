f08b1fb68ba8be5cf6f2613a5db50e364f0899bb
GoldDeniel <aranyadhun@gmail.com>
2024-11-16 21:30:43 +0100

    :fire: error reporting

--- app/core/init.php
+++ app/core/init.php
@@ -1,9 +1,10 @@
 <?php 
-
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 require 'functions.php';
 require 'Controller.php';
-require 'Model.php';
 require 'Database.php';
+require 'Model.php';
 require 'config.php';
 require 'App.php';
 
