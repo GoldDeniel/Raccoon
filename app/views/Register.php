c74bef8a01b215ee29c4787dea47536dc1bc02e2
GoldDeniel <aranyadhun@gmail.com>
2024-11-17 12:38:16 +0100

    :rocket: added registration

--- app/views/Register.php
+++ app/views/Register.php
@@ -8,10 +8,13 @@
     <?php include_once '../app/views/elements/header.php'; ?>
     <div>
         <h1>Register</h1>
-        <form action="/Raccoon/public/login" method="POST">
+        <form action="/Raccoon/public/register/register" method="POST">
             
-            <label for="name">Name:</label>
-            <input type="text" name="name" id="name" required>
+            <label for="first_name">First Name:</label>
+            <input type="text" name="first_name" id="first_name" required>
+
+            <label for="last_name">Last Name:</label>
+            <input type="text" name="last_name" id="last_name" required>
 
             <label for="username">Username:</label>
             <input type="username" name="username" id="username" required>
@@ -20,6 +23,9 @@
             <input type="password" name="password" id="password" required>
 
             <button type="submit">Register</button>
+            <?php if (isset($data['error'])): ?>
+                <p style="color: red;"><?php echo htmlspecialchars($data['error']); ?></p>
+            <?php endif; ?>
         </form>
     </div>
 </body>
