4af68bb397a81389bc7f72013c394112cf6179ba
GoldDeniel <aranyadhun@gmail.com>
2024-11-17 12:51:07 +0100

    :lock: secured feed

--- app/controllers/FeedController.php
+++ app/controllers/FeedController.php
@@ -6,6 +6,10 @@ class FeedController extends Controller
 
 	public function index()
 	{
+		if (!isset($_SESSION['user_id'])) {
+			header('Location: '.ROOT.'/login');
+			return;
+		}
 		$postModel = new Post();
         $posts = $postModel->getAllPosts();
         $this->view('Feed', ['Posts' => $posts]);
